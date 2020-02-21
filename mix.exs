c1b97b8ec3e43387157ded1341c22cc87c43c0c9
Sean Esopenko <sean@localhost.localdomain>
2020-02-21 11:50:15 -0700

    Increment hex package

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Sesopenko.ECS.MixProject do
   def project do
     [
       app: :sesopenko_ecs,
-      version: "0.1.1",
+      version: "0.1.3",
       elixir: "~> 1.9",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
